7653d1a1c1591b1f440bfd46f14d53ced0154503
hedaukartik <hedaukartik2009@gmail.com>
2018-12-01 20:30:56 +0530

    Created Navbar, Dashboard before Firebase Connectivity

---
+++ moneymen/src/components/clients/Clients.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Clients extends Component {
+  render() {
+    const clients = [
+      {
+        id: "13423324",
+        firstName: "Kartik",
+        lastName: "Hedau",
+        email: "hedaukartik2009@gmail.com",
+        phone: "4444444",
+        balance: "30"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                <i className="fas fa-users" />
+                Users
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>
+                    {" "}
+                    <i className="fas fa-rupee-sign" />{" "}
+                    {parseFloat(client.balance).toFixed(2)}
+                  </td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" />
+                      Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading..</h1>;
+    }
+    return (
+      <div>
+        <h1>Clients</h1>
+      </div>
+    );
+  }
+}
+
+export default Clients;
